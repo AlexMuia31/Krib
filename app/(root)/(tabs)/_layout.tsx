722b787355a6de5a979bcecc2185bae8edf1f67f
AlexMuia31 <alexmuia31@gmail.com>
2026-05-01 20:23:59 +0300

    added tabs for android and Ios

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,18 +1,64 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { Platform } from "react-native";
 import "../../../global.css";
 
 export default function TabLayout() {
+  if (Platform.OS === "android") {
+    return (
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="saved"
+          options={{
+            title: "",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="heart" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    );
+  }
+
+  // iOS – keep your NativeTabs with SF Symbols
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
         <Icon sf="house.fill" />
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="settings">
+      <NativeTabs.Trigger name="search">
         <Label>Search</Label>
         <Icon sf="magnifyingglass" />
       </NativeTabs.Trigger>
-      {/* create Property */}
       <NativeTabs.Trigger name="saved">
         <Label>Saved</Label>
         <Icon sf="heart.fill" />
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,6 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return (
-    <SafeAreaView className="bg-red-500 p-4">
-      <View>
-        <Text className="text-8xl">
-          Edit app/index.tsx to edit this screen.
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Slot />;
 }
