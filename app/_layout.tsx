6232a3b29d29f1c418bf449302a0d2928941821f
AlexMuia31 <alexmuia31@gmail.com>
2026-05-01 18:33:59 +0300

    fixed tailwind errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
-import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView className="bg-red-500 p-4">
+      <View>
+        <Text className="text-8xl">
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
