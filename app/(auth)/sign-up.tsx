300af8349383a927dc1003a7c054d66f07198454
AlexMuia31 <alexmuia31@gmail.com>
2026-05-04 14:10:59 +0300

    sign up done

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,4 +1,4 @@
-import { useSignUp } from "@clerk/expo";
+import { useAuth, useSignUp } from "@clerk/expo";
 import { Link, useRouter } from "expo-router";
 import React from "react";
 import {
@@ -20,9 +20,15 @@ export default function SignUp() {
   const [password, setPassword] = React.useState("");
   const [code, setCode] = React.useState("");
 
+  const { isSignedIn } = useAuth();
+
   const router = useRouter();
 
   const isLoading = fetchStatus === "fetching";
+
+  if (signUp.status === "complete" || isSignedIn) {
+    return;
+  }
 
   const onSignUpPress = async () => {
     const { error } = await signUp.password({
@@ -38,6 +44,70 @@ export default function SignUp() {
     }
     if (!error) await signUp.verifications.sendEmailCode();
   };
+
+  const onVerifyPress = async () => {
+    await signUp.verifications.verifyEmailCode({ code });
+
+    if (signUp.status === "complete") {
+      await signUp.finalize({
+        navigate: ({ decorateUrl }) => {
+          const url = decorateUrl("/");
+          router.replace(url as any);
+        },
+      });
+    }
+  };
+
+  if (
+    signUp.status === "missing_requirements" &&
+    signUp.unverifiedFields.includes("email_address") &&
+    signUp.missingFields.length === 0
+  ) {
+    return (
+      <View className="flex-1 justify-center px-6 py-12">
+        <Image
+          source={require("../../assets/images/kribb.png")}
+          className="w-32 h-16 mb-8"
+          resizeMode="contain"
+        />
+        <Text className="text-3xl font-bold text-gray-800 mb-2">
+          Verify Your Account
+        </Text>
+        <Text>we've sent an email to {email}</Text>
+
+        <TextInput
+          className="w-full border border-gray-300 rounded-xl px-4 py-3 mt-4"
+          placeholder="Enter verification code"
+          placeholderTextColor="#9ca3af"
+          keyboardType="number-pad"
+          value={code}
+          onChangeText={setCode}
+        />
+        {errors.fields.emailAddress && (
+          <Text className="text-red-500 mb-4">
+            {errors.fields.emailAddress.message}
+          </Text>
+        )}
+        <TouchableOpacity
+          onPress={onVerifyPress}
+          disabled={isLoading}
+          className="bg-blue-500 rounded-xl py-3 mt-4"
+        >
+          {isLoading ? (
+            <ActivityIndicator color="white" />
+          ) : (
+            <Text className="text-white text-center font-bold">Verify</Text>
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => signUp.verifications.sendEmailCode()}
+          className=" py-3 mt-1"
+        >
+          <Text className="text-blue-600 font-bold">Resend Code</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
     <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
       <ScrollView
