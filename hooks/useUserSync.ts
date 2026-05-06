5869aaad7193271cb4e27618ee812cf1af2345ab
AlexMuia31 <alexmuia31@gmail.com>
2026-05-06 15:07:07 +0300

    sync user

--- hooks/useUserSync.ts
+++ hooks/useUserSync.ts
@@ -1,7 +1,30 @@
 import { useUserStore } from "@/store/userStore";
 import { useUser } from "@clerk/expo";
+import { useEffect } from "react";
+import { useSupabase } from "./useSupabase";
 
 export const useUserSync = () => {
   const { user } = useUser();
   const setIsAdmin = useUserStore((state) => state.setIsAdmin);
+
+  const authSupabase = useSupabase();
+
+  useEffect(() => {
+    if (!user) return;
+    syncUser();
+  }, [user]);
+
+  const syncUser = async () => {
+    const { data } = await authSupabase
+      .from("users")
+      .select("clerk_id,is_admin")
+      .eq("clerk_id", user!.id)
+      .single();
+
+    if (data) {
+      // if the user exists in the database, set the isAdmin state based on the database value
+      setIsAdmin(data.is_admin ?? false);
+      return;
+    }
+  };
 };
