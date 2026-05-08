57169e84e00995dbf44ed0a3fea0a8b3cd62bcc3
AlexMuia31 <alexmuia31@gmail.com>
2026-05-08 12:06:48 +0300

    adding property card

---
+++ components/PropertyCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function PropertyCard() {
+  return (
+    <View>
+      <Text>PropertyCard</Text>
+    </View>
+  );
+}
